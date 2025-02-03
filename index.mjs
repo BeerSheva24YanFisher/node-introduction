1cdfaa8fe0a07d2d76f91efc4bde65e35ef16936
yanfi-sher <loss.soul20@gmail.com>
2025-02-03 10:21:37 +0200

    winston

---
+++ index.mjs
@@ -0,0 +1,6 @@
+
+import logger from './logger.mjs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+console.log('from module', process.env.LEVEL);
+logger.info(logger.level);//access __filename from ECS module
