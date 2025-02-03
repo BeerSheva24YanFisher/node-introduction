1bf36600ec1e1f4e63cea72c4d5be9784d827528
yanfi-sher <loss.soul20@gmail.com>
2025-02-03 10:27:36 +0200

    file-system

---
+++ index.mjs
@@ -0,0 +1,6 @@
+
+import logger from './logger.mjs';
+import fs from 'node:fs'
+// const data = fs.readFileSync('./index.mjs');
+// logger.info(data);
+fs.writeFileSync('./file.txt', ["kuku", "kukureku", "Hello World!"].join('\n'))
